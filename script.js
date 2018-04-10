470515febc8f345f266f0ec8a2ad00196918131a
Yulia Litvinenko <julia-litvinenko@yandex.ru>
2018-04-11 00:17:31 +0300

    New Form alert message

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ formButton.onclick = function() {
 event.preventDefault();
 const name = document.getElementById('name');
 const email = document.getElementById('Email');
+const form = document.getElementById('form');
+const message = document.getElementById('formMessage');
 let className = name.classList;
 let classEmail = email.classList;
+let classForm = form.classList;
+let classMessage = message.classList;
 if (name.value === "") {
 	className.add ("alert");
   } else {className.remove ("alert");
@@ -15,6 +19,11 @@ if (email.value === "") {
   } else {classEmail.remove ("alert");
   }
 if (name.value != "" && email.value != "") {
-	alert("Thank you, your message is sent!")
+	classForm.add ("form__submitted");
+	classForm.remove ("form");
+	classMessage.add ("form__message");
+	classMessage.remove ("message__notsubmitted");
   }
+console.log (classMessage);
+console.log (classForm);
 }
